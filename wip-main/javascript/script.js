41dee89be394b5360d0a43b0214de3d1e24ebfd6
Ggogua <giorgiggogua@gmail.com>
2023-12-28 19:58:56 +0400

    git add "updated"

---
+++ wip-main/javascript/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+        rewind     : true,
+        type       : 'fade',
+        heightRatio: 0.5,
+        autoplay   : true,  
+        interval   : 3000,
+        pagination: false
+    }).mount();
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputFields = document.querySelectorAll(".input-field");
+
+    inputFields.forEach(inputField => {
+        inputField.addEventListener('click', (event) => {
+            event.preventDefault();
+            event.stopPropagation(); 
+
+            inputField.classList.toggle('style');
+
+            inputFields.forEach(otherInputField => {
+                if (otherInputField !== inputField) {
+                    otherInputField.classList.remove('style');
+                }
+            });
+        });
+    });
+
+
+    document.addEventListener('click', () => {
+        inputFields.forEach(inputField => {
+            inputField.classList.remove('style');
+        });
+    });
+});
+
+
